fd20656089e2662c4490fb327da64060b342a63e
varshiniThillai <74129727+varshiniThillai@users.noreply.github.com>
2021-04-30 13:02:14 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,32 @@
+class Launcher{
+    constructor(body, anchor){
+        var options = {
+            bodyA: body,
+            pointB: anchor,
+            stiffness: 0.004,
+            length: 1,
+        }
+        this.bodyA=body;
+        this.pointB=anchor;
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    fly(){
+        this.launcher.bodyA=null;
+    }
+
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+
+    display(){
+     if(this.launcher.bodyA){
+         var pointA=this.bodyA.position
+         var pointB=this.pointB
+         strokeWeight(2)
+         line(pointA.x,pointA.y,pointB.x,pointB.y)
+     }  
+    }
+    
+}
